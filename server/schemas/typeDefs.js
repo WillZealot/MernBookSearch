9b2082f53eb9a9d41aabbb7cd52dca158946d5ba
William Tikhonenko <williamtikhonenko@gmail.com>
2023-09-16 17:27:59 -0400

    ability to connect to apollo graphql established time to create proper files

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,16 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type User {
+    id: ID
+    username: String
+    email: String
+    
+}
+
+type Query {
+    users: [User]
+  }
+
+`
+module.exports = typeDefs;
