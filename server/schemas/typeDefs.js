7cede15d8a2efb7a0e15685bbb5bf4e7be08e1dc
William Tikhonenko <williamtikhonenko@gmail.com>
2023-09-16 17:40:22 -0400

    ability to connect is good i also wrote some queries but i cant really test them due to no users being seeds or even created so...

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,11 +1,20 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+type Book {
+    bookId: ID
+    title: String
+    authors: String
+    description: String
+    image: String
+    link: String
+}
+
 type User {
     id: ID
     username: String
     email: String
-    
+    savedBooks: [Book]
 }
 
 type Query {
