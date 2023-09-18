8da44afb8554ce5328177fbd39bb894426273037
William Tikhonenko <williamtikhonenko@gmail.com>
2023-09-18 18:48:30 -0400

    added some files in the client utils to orientate myself with comments aswell

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,5 @@
+import { gql } from '@apollo/client';
+// put the queries that apollo is giving you here to create the user and what info you want returned
+// we are creating these to replace essentially the whole api hit system
+export const CREATE_USER = gql`
+`;
---
+++ client/src/utils/queries.js
@@ -0,0 +1,6 @@
+import { gql } from '@apollo/client';
+// put the queries that apollo is giving you here to create the user and what info you want returned
+// we are creating these to replace essentially the whole api hit system
+export const QUERY_USER = gql`
+
+`;
